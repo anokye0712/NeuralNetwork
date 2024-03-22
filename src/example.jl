a84640c8d44f7c5f03858192be753c296bb58529
Anokye <anokye561@outlook.com>
2024-03-22 14:36:51 +0800

    Separate functions from main module

--- src/example.jl
+++ src/example.jl
@@ -3,8 +3,8 @@ using Revise
 includet("NeuralNetworks.jl")
 using .NeuralNetworks
 
-training, validation, testing = loadData()
 net = Network([784, 30, 10])
+training, validation, testing = loadData()
 SGD(net, training, validation, 10, 1.0, 20)
 
 begin
